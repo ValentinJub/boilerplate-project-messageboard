953b9e5197afcdbdfdc3e03121e31b961a74b678
Valentin <valentinwissler42@outlook.com>
2023-04-28 18:22:26 +0100

    boilerplate working

--- server.js
+++ server.js
@@ -3,41 +3,64 @@ require('dotenv').config();
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
+const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override');
 
-const apiRoutes         = require('./routes/api.js');
+
+const threadsRouter = require('./routes/threads.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
+const mongoose          = require('mongoose');
+const helmet            = require('helmet');
 
 const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+mongoose.connect(process.env.DB).then(
+  () => { console.log('Connected to database')},
+  (err) => { console.log('Error connecting to database', err)}
+);
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+var path = require ('path');
+app.use(express.static(path.join(__dirname + '../public')));
+
+app.set('view engine', 'ejs');
+app.set('views, __dirname + ./views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(methodOverride('_method'));
+
+
+
 
 //Sample front-end
 app.route('/b/:board/')
   .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/board.html');
+    res.render('board');
   });
+
 app.route('/b/:board/:threadid')
   .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/thread.html');
+    res.render('thread')
   });
 
 //Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.render('index');
   });
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);
+app.use('/api/threads', threadsRouter)
+
+
+
 
 //404 Not Found Middleware
 app.use(function(req, res, next) {
